bddd617ed51cf5ebde43a85bc6cdbb6edbcdeef9
oigabrielteodoro <oi@gabrielteodoro.com>
2021-04-24 17:13:19 -0300

    feat: added user storaged in Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useEffect, useState } from 'react';
 
 import avatarImg from '../../assets/avatar.jpeg';
 
 import { Container, UserInformation, HelloText, UserName, UserAvatar } from './styles';
 
 export function Header() {
+  const [username, setUsername] = useState<string>();
+
+  useEffect(() => {
+    async function loadUsername() {
+      const user = await AsyncStorage.getItem('@plantmanager:user');
+
+      setUsername(user || '');
+    }
+
+    loadUsername();
+  }, []);
+
   return (
     <Container>
       <UserInformation>
         <HelloText>Olá,</HelloText>
-        <UserName>Gabriel</UserName>
+        <UserName>{username}</UserName>
       </UserInformation>
 
       <UserAvatar source={avatarImg} />
